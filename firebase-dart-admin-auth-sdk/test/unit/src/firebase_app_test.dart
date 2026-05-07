37d29788adb61354e3c48c0b810ebc4204617af8
Jeremy Andrews <jandrews@venturseed.com>
2026-05-07 19:19:12 -0400

    test(firebase): restore disabled unit tests
    Signed-off-by: Jeremy Andrews <jandrews@venturseed.com>

--- firebase-dart-admin-auth-sdk/test/unit/src/firebase_app_test.dart
+++ firebase-dart-admin-auth-sdk/test/unit/src/firebase_app_test.dart
@@ -1,22 +1,36 @@
-// import 'package:firebase_dart_admin_auth_sdk/firebase_dart_admin_auth_sdk.dart';
-// import 'package:test/test.dart';
+import 'package:ds_tools_testing/ds_tools_testing.dart';
+import 'package:firebase_dart_admin_auth_sdk/firebase_dart_admin_auth_sdk.dart';
 
-// void main() {
-//   setUp(() {
-//     FirebaseApp.resetForTests();
-//   });
+void main() {
+  setUp(FirebaseApp.resetForTests);
+  tearDown(FirebaseApp.resetForTests);
 
-//   test('FirebaseApp initializes with env variables', () async {
-//     final app = await FirebaseApp.initializeAppWithEnvironmentVariables(
-//       apiKey: 'fake_api_key',
-//       authdomain: 'fake.firebaseapp.com',
-//       projectId: 'fake_project',
-//       messagingSenderId: '123456',
-//       bucketName: 'fake.appspot.com',
-//       appId: 'app-123',
-//     );
+  test('FirebaseApp initializes with environment variables', () async {
+    final app = await FirebaseApp.initializeAppWithEnvironmentVariables(
+      apiKey: 'fake_api_key',
+      authdomain: 'fake.firebaseapp.com',
+      projectId: 'fake_project',
+      messagingSenderId: '123456',
+      bucketName: 'fake.appspot.com',
+      appId: 'app-123',
+    );
 
-//     expect(app, isNotNull);
-//     expect(app.getAuth(), isNotNull);
-//   });
-// }
+    expect(app, same(FirebaseApp.instance));
+    expect(app.getAuth(), isNotNull);
+  });
+
+  test('FirebaseApp resetForTests clears singleton state', () async {
+    await FirebaseApp.initializeAppWithEnvironmentVariables(
+      apiKey: 'fake_api_key',
+      authdomain: 'fake.firebaseapp.com',
+      projectId: 'fake_project',
+      messagingSenderId: '123456',
+      bucketName: 'fake.appspot.com',
+      appId: 'app-123',
+    );
+
+    FirebaseApp.resetForTests();
+
+    expect(() => FirebaseApp.instance, throwsA(isA<StateError>()));
+  });
+}
